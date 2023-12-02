75d5b3fb0189e31ef46dd14fcbf7388de81bb492
Spas Angelov <spasangelov@gmail.com>
2023-12-02 12:34:59 -0500

    Black Formatter

--- backend/src/core/router_manager.py
+++ backend/src/core/router_manager.py
@@ -5,9 +5,8 @@ from ..api.v1.user.create import router as create_user_router
 from ..api.v1.user.auth import router as auth_router
 from ..api.v1.blog.posts import router as blog_router
 
+
 def inlcude_all_routers(app: FastAPI):
     app.include_router(create_user_router, prefix="/api/v1/user")
     app.include_router(auth_router)
     app.include_router(blog_router, prefix="/api/v1/blog")
-
-
